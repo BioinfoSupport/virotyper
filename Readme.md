590d667b7bb681b3267248d3cab931910bf06b1d
Julien Prados <julien.prados@unige.ch>
2024-08-22 17:21:32 +0200

    improve database cheching and README

--- Readme.md
+++ Readme.md
@@ -11,24 +11,46 @@ DEALINGS IN THE SOFTWARE.
 
 # Usage
 
-## In the cloud
+- prerequisite: a resistance database must exist in folder `data/<DBID>/db`, see section below for the format.
 
-[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/BioinfoSupport/virotyper.git/HEAD?urlpath=rstudio)
-
-[![Binder](http://mybinder.org/badge_logo.svg)](http://mybinder.org/v2/gh/BioinfoSupport/virotyper.git/HEAD?urlpath=shiny/app)
-
-
-# Locally
 
 ```bash
-repo2docker --Repo2Docker.platform=linux/amd64 ./
+# Build the docker image
+docker build -t virotyper ./
+
+# Run a bash in the container
+docker run --rm -it -v $(pwd):/data --workdir /data virotyper bash
 ```
 
+
+
+# Resistance database
+
+A resistance database is a subdirectory located in `data/db/<DB_ID>/` with the following content:
+```
+data/db/hsv1/
+  - ref.fasta         reference sequence(s)
+  - ref.gff           reference annotation (optional: generated from ref.fasta if missing, assuming each sequence is a gene)
+  - resistances.xlsx  an excel containing known resitances   
+  - template.docx     a template word document used to generate the resistance report
+```
+
+
+`resistances.xlsx` must be an excel file containing a sheet named `resistances` with at least columns:
+
+ - `strain_id`: format is `UL23-HHV2:A19V` to encode a mutation on the 19th amino-acid of gene `UL23-HHV2` which is `A` in the reference genome, and became a `V` in the mutated strain. `UL23-HHV2:t42a` encode a DNA mutation at nucleotide 42 which is `t` in the reference and become a `a` in the mutated strain. To encode the insertion of a `g` at position 366 on gene `UL23-HHV2` the format is `UL23-HHV2:t365tg` (Note: the mutation refer to nucleotide before). Similarly to encode the deletion of sequence `ac` at position 862 on gene `UL23-HHV2`, the format is: `UL23-HHV2:cac861c`.
+ - `mutation_type`: Type of mutation (Either: Natural, Resistant, Susceptible)
+ - `drug`: Name of the drug the mutation as an impact on (missing if mutation type is Natural).
+ 
+The same mutation can appear several time in the file if it confere a resistance to multiple drugs.
+
+
+
+
+
+# Parking
+
 ```bash
-# Run a bash in the container
-docker run --rm -it -v $(pwd):/data --workdir /data unigebsp/virotyper bash
-
-
 # Clean reporting files in the given directory
 make data/HHV1/bam/clean
 make data/HHV1/fasta/clean
@@ -36,64 +58,20 @@ make data/HHV2/bam/clean
 make data/HHV2/fasta/clean
 
 # Run variant calling from a single existing BAM file
-make data/HHV1/bam/240222-HE.HHV1.filt.10k.report
-make DB_DIR=data/HHV2/db data/HHV2/bam/240222-RO.HHV2.filt.10k.report
+make data/HHV1/bam/240222-HE.HHV1.filt.10k.bam.all
+make DB_ID=hsv2 data/HHV2/bam/240222-RO.HHV2.filt.10k.bam.all
 
 # Run variant calling for all BAM files in a directory
-ls data/HHV1/bam/*.bam | grep -v filt | sed s/.bam$/.filt.10k.report/ | xargs make -kj6
-ls data/HHV2/bam/*.bam | grep -v filt | sed s/.bam$/.filt.10k.report/ | xargs make -j6 DB_DIR=data/HHV2/db
+ls data/HHV1/bam/*.bam | grep -v filt | sed s/.bam$/.filt.10k.bam.all/ | xargs make
+ls data/HHV1/bam/*.filt.10k.bam | sed s/.bam$/.vcf.gz.all/ | xargs make
+ls data/HHV2/bam/*.bam | grep -v filt | sed s/.bam$/.filt.10k.bam.all/ | xargs make DB_DIR=data/HHV2/db
 
 # Run variant calling for all FASTA files in a directory
-ls data/HHV1/fasta/*.fasta | sed s/$/.all/ | xargs make -kj6
-ls data/HHV2/fasta/*.fasta | sed s/.fasta$/.asm.report/ | xargs make -kj6 DB_DIR=data/HHV2/db
+ls data/HHV1/fasta/*.fasta | sed s/$/.all/ | xargs make
+ls data/HHV2/fasta/*.fasta | sed s/$/.all/ | xargs make DB_DIR=data/HHV2/db
 
 # Generate reports for all VCF files in a directory
 ls data/HHV1/vcf/*.vcf.gz data/HHV1/vcf/*.bam | sed s/$/.all/ | xargs make -kj6
-```
-
-
-
-
-# Build the image
-```bash
-docker build -t unigebsp/virotyper ./
-```
-
-
-# Parking
-
-
-
-```bash
-# Query mutations of a given strain in the database
-bcftools view --private --samples 'HSV1-UL30:L702H:1:T>A' /repository/pipeline/ref/hsv1.db.vcf.bgz
-bcftools view --private --samples 'HSV1-UL30:A910V:1:C>T' /repository/pipeline/ref/hsv1.db.vcf.bgz
-
-# Generate some consensus sequence from the DB to test the pipeline
-bcftools consensus --sample 'HSV1-UL30:L702H:1:T>A' --fasta /repository/pipeline/ref/hsv1.fasta /repository/pipeline/ref/hsv1.db.vcf.bgz > out/tests/consensus01.fasta
-
-
-# Merge multiple VCF into a single one
-bcftools merge -Oz --force-samples out/tests/{consensus01,consensus02}.hsv1.mm2.csq.ann.vcf.bgz > out/ab.hsv1.mm2.csq.ann.vcf.bgz
-bcftools index out/ab.hsv1.mm2.csq.ann.vcf.bgz
-bcftools view out/ab.hsv1.mm2.csq.ann.vcf.bgz
-make -f /repository/pipeline/Makefile out/ab.hsv1.mm2.csq.ann.csq.ann.vcf.bgz
-bcftools query -f'[%CHROM\t%POS\t%SAMPLE\t%TBCSQ\n]' out/ab.hsv1.mm2.csq.ann.csq.ann.vcf.bgz
-```
-
-
-## IGV
-
-Example IGV script to generate screenshots of given locus
-```bash
-./IGV_2.16.2/igv.sh --genome ./pipeline/ref/hsv1.fasta --batch /dev/stdin <<EOF
-load ./pipeline/ref/hsv1.gff
-load data/HHV1_UL23_UL30_231108.hsv1.mm2.bam
-colorBy READ_STRAND
-goto JN555585.1-UL30:2105 JN555585.1-UL30:2729
-expand hsv1.gff
-snapshot out/snapshot.png
-EOF
 ```
 
 
--- app.R
+++ app.R
@@ -5,175 +5,187 @@ library(bsicons)
 
 options(shiny.maxRequestSize=5000*1024^2)
 
-shinyApp(
-  ui = page_navbar(
+
+ui_vcf_panel <- function() {
+  nav_panel(
+    title = "VCF",
+    tags$p("Use this form to generate a resistance report from a VCF file."),
+    inputPanel(
+      title = "Generate a report from a VCF containing variants",
+      selectInput("resistance_db","Resistance database",choices = c("HSV1"="data/HHV1/db","HSV2"="data/HHV2/db")),
+      fileInput("vcf_file","VCF file",accept = c(".vcf",".gz"),multiple = FALSE,placeholder = "Upload a VCF file (with INFO fields DP,AF,BCSQ)"),
+    ),
+    shinyjs::disabled(actionButton("btn_make_vcf_report","Generate VCF Report",icon = icon("cog"))),
+    shinyjs::disabled(actionButton("btn_view_vcf_report","View HTML report",icon = icon("eye"))),
+    shinyjs::disabled(downloadButton("btn_dl_vcf_report_docx","Download DOCX Report",icon = icon("download")))
+  )
+}
+
+ui_bam_panel <- function() {
+  nav_panel(
+    title = "BAM",
+    tags$p("Use this form to generate a report from a BAM file containing ONT reads aligned on a reference"),
+    inputPanel(
+      fileInput("bam_file","BAM file",accept = ".bam",multiple = FALSE,placeholder = "Upload BAM file of aligned reads on the reference",width="100%")
+    ),
+    shinyjs::disabled(actionButton("btn_make_bam_report","Generate BAM Report",icon = icon("cog"))),
+    shinyjs::disabled(actionButton("btn_view_bam_report","View BAM Report",icon = icon("eye")))
+  )
+}
+
+ui_fasta_panel <- function() {
+  nav_panel(
+    title="FASTA",
+    markdown("Use this form to detect mutations in a consensus sequence relatively to a reference database.
+                This app align the consensus sequences in the given FASTA file on the reference sequence using `minimap2`, and 
+                detect the variants with `bcftools`."),
+    inputPanel(
+      tags$p(tags$b("Not available yet !!!"))
+    )
+  )
+}
+
+ui <- function() {
+  page_navbar(
     title = icon("biohazard"),
     theme = bs_theme(),
     collapsible = TRUE,
     inverse = TRUE,
     header = shinyjs::useShinyjs(),
     footer = card(max_height="200px",
-      class="bg-warning",
-      card_header("Disclaimer"),
-      card_body("IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
-        ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
-        TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
-        OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
-      )
+                  class="bg-warning",
+                  card_header("Disclaimer"),
+                  card_body("IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+          ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+          TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+          OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
+                  )
     ),
-    nav_panel(
-      title = "VCF",
-      tags$p("Use this form to generate a resistance report from a VCF file."),
-      inputPanel(
-        title = "Generate a report from a VCF containing variants",
-        selectInput("resistance_db","Resistance database",choices = c("HSV1"="data/HHV1/db","HSV2"="data/HHV2/db")),
-        fileInput("vcf_file","VCF file",accept = c(".vcf",".gz"),multiple = FALSE,placeholder = "Upload a VCF file (with INFO fields DP,AF,BCSQ)"),
-      ),
-      shinyjs::disabled(actionButton("btn_make_vcf_report","Generate VCF Report",icon = icon("cog"))),
-      shinyjs::disabled(actionButton("btn_view_vcf_report","View HTML report",icon = icon("eye"))),
-      shinyjs::disabled(downloadButton("btn_dl_vcf_report_docx","Download DOCX Report",icon = icon("download")))
-    ),
-    nav_panel(
-      title = "BAM",
-      tags$p("Use this form to generate a report from a BAM file containing ONT reads aligned on a reference"),
-      inputPanel(
-        fileInput("bam_file","BAM file",accept = ".bam",multiple = FALSE,placeholder = "Upload BAM file of aligned reads on the reference",width="100%")
-      ),
-      shinyjs::disabled(actionButton("btn_make_bam_report","Generate BAM Report",icon = icon("cog"))),
-      shinyjs::disabled(actionButton("btn_view_bam_report","View BAM Report",icon = icon("eye")))
-    ),
-    nav_panel(
-      title="FASTA",
-      markdown("Use this form to detect mutations in a consensus sequence relatively to a reference database.
-              This app align the consensus sequences in the given FASTA file on the reference sequence using `minimap2`, and 
-              detect the variants with `bcftools`."),
-      inputPanel(
-        tags$p(tags$b("Not available yet !!!"))
-      )
+    ui_vcf_panel(),
+    ui_bam_panel(),
+    ui_fasta_panel()
     #   selectInput("resistance_db","Resistance database",choices = c("HSV1","HSV2")),
     #   textAreaInput("fasta_seq","DNA sequences (FASTA)",placeholder = ">UL23\nATGCTCATGACTGCATCTA...\n>UL30\nATGCTCATGACTGCATCTA...",height = "400px"),
-    #   actionButton("make_doc","Report.doc",icon = icon("download")),
     #   shinycssloaders::withSpinner(textOutput("terminal_stdout",container = pre),type = 1)
-    )
-  ),
+  )
+}
 
-  server = function(input, output, session) {
-    
-    
-    
-    #-#-#-#-#-#-#-#
-    # BAM
-    #-#-#-#-#-#-#-#
-    bam_report_dir <- reactiveVal(NULL)
-    
-    observeEvent(input$bam_file,{
-      validate(need(is.character(input$bam_file$datapath),"invalid BAM"))
-      validate(need(file.exists(input$bam_file$datapath),"invalid BAM"))
-      shinyjs::enable("btn_make_bam_report")
-    })
-    
-    observeEvent(input$btn_make_bam_report,{
-      validate(need(is.character(input$bam_file$datapath),"invalid BAM"))
-      shinyjs::disable("btn_view_bam_report")
-      shinyjs::disable("btn_dl_bam_report_html")
-      
-      # Copy inputs to temp location
-      tempdir <- tempdir()
-      file.copy("notebooks/bam_report.Rmd", file.path(tempdir,"template.Rmd"), overwrite = TRUE)
-      file.copy(input$bam_file$datapath, file.path(tempdir,input$bam_file$name), overwrite = TRUE)
-      
-      # Generate report
-      withProgress(message="Generate BAM report",{
-        rmarkdown::render(
-          file.path(tempdir,"template.Rmd"),
-          output_file = file.path(tempdir,"bam_report.html"),
-          params = list(input_bam_file=file.path(tempdir,input$bam_file$name)),
-          envir = new.env(parent = globalenv())
-        )
-      })
-      bam_report_dir(tempdir)
-      shinyjs::enable("btn_view_bam_report")
-      shinyjs::enable("btn_dl_bam_report_html")
-    })
-    
-    observeEvent(input$btn_view_bam_report,{
-      showModal(modalDialog(
-        title = "BAM report",
-        tags$iframe(srcdoc=readLines(file.path(bam_report_dir(),"bam_report.html")), style='width:100%;height:600px;'),
-        easyClose = TRUE,size = "xl",
-        footer = downloadButton("btn_dl_bam_report_html","Download",icon = icon("download"),class="btn-primary")
-      ))
-    })
-    
-    output$btn_dl_bam_report_html <- downloadHandler(
-      filename = function(){paste0(input$bam_file$name,".html")},
-      content = function(file) {file.copy(file.path(bam_report_dir(),"bam_report.html"),file)}
-    )
-      
-    #-#-#-#-#-#-#-#
-    # VCF
-    #-#-#-#-#-#-#-#
-    vcf_report_dir <- reactiveVal(NULL)
-    
-    observeEvent(input$vcf_file,{
-      validate(need(is.character(input$vcf_file$datapath),"invalid VCF"))
-      shinyjs::enable("btn_make_vcf_report")
-    })
-    
-    observeEvent(input$btn_make_vcf_report,{
-      validate(need(is.character(input$vcf_file$datapath),"invalid VCF"))
-      shinyjs::disable("btn_view_vcf_report")
-      shinyjs::disable("btn_dl_vcf_report_docx")
-      
-      # Copy inputs to temp location
-      tempdir <- tempdir()
-      file.copy("notebooks/vcf_report.Rmd", file.path(tempdir,"template.Rmd"), overwrite = TRUE)
-      file.copy(input$vcf_file$datapath, file.path(tempdir,input$vcf_file$name), overwrite = TRUE)
-      file.copy(input$resistance_db, tempdir, overwrite = TRUE,recursive = TRUE)
-      
-      # Generate report
-      withProgress(message="Generate VCF report",{
-        rmarkdown::render(
-          file.path(tempdir,"template.Rmd"),
-          output_file = file.path(tempdir,"vcf_report.html"),
-          params = list(
-            input_vcf_file = file.path(tempdir,input$vcf_file$name),
-            input_db_dir = file.path(tempdir,"db"),
-            output_docx_report = file.path(tempdir,"vcf_report.docx")),
-          envir = new.env(parent = globalenv())
-        )
-      })
-      shinyjs::enable("btn_view_vcf_report")
-      shinyjs::enable("btn_dl_vcf_report_docx")
-      bam_report_dir(tempdir)
-    })
-    
-    observeEvent(input$btn_view_vcf_report,{
-      showModal(modalDialog(
-        title = "VCF report",
-        tags$iframe(srcdoc=readLines(file.path(bam_report_dir(),"vcf_report.html")), style='width:100%;height:600px;'),
-        easyClose = TRUE,size = "xl",
-        footer = downloadButton("btn_dl_vcf_report_html","Download",icon = icon("download"),class="btn-primary")
-      ))
-    })
+server <- function(input, output, session) {
 
-    output$btn_dl_vcf_report_html <- downloadHandler(
-      filename = function(){paste0(input$vcf_file$name,".html")},
-      content = function(file) {file.copy(file.path(bam_report_dir(),"vcf_report.html"),file)}
-    )
-
-    output$btn_dl_vcf_report_docx <- downloadHandler(
-      filename = function(){paste0(input$vcf_file$name,".docx")},
-      content = function(file) {file.copy(file.path(bam_report_dir(),"vcf_report.docx"),file)}
-    )
+  #-#-#-#-#-#-#-#
+  # BAM
+  #-#-#-#-#-#-#-#
+  bam_report_dir <- reactiveVal(NULL)
+  
+  observeEvent(input$bam_file,{
+    validate(need(is.character(input$bam_file$datapath),"invalid BAM"))
+    validate(need(file.exists(input$bam_file$datapath),"invalid BAM"))
+    shinyjs::enable("btn_make_bam_report")
+  })
+  
+  observeEvent(input$btn_make_bam_report,{
+    validate(need(is.character(input$bam_file$datapath),"invalid BAM"))
+    shinyjs::disable("btn_view_bam_report")
+    shinyjs::disable("btn_dl_bam_report_html")
     
-    # output$terminal_stdout <- eventReactive(input$make_doc,{
-    #   validate(need(is.character(input$vcf_file$datapath),"invalid fasta"))
-    #   cmd <- sprintf("make %s 2>&1",input$vcf_file$datapath)
-    #   print(cmd)
-    #   withProgress(message="Generate report",{
-    #     pipe(cmd) |> readLines() |> paste(collapse = "\n")
-    #   })
-    # })
-  }
-)
+    # Copy inputs to temp location
+    tempdir <- tempdir()
+    file.copy("notebooks/bam_report.Rmd", file.path(tempdir,"template.Rmd"), overwrite = TRUE)
+    file.copy(input$bam_file$datapath, file.path(tempdir,input$bam_file$name), overwrite = TRUE)
+    
+    # Generate report
+    withProgress(message="Generate BAM report",{
+      rmarkdown::render(
+        file.path(tempdir,"template.Rmd"),
+        output_file = file.path(tempdir,"bam_report.html"),
+        params = list(input_bam_file=file.path(tempdir,input$bam_file$name)),
+        envir = new.env(parent = globalenv())
+      )
+    })
+    bam_report_dir(tempdir)
+    shinyjs::enable("btn_view_bam_report")
+    shinyjs::enable("btn_dl_bam_report_html")
+  })
+  
+  observeEvent(input$btn_view_bam_report,{
+    showModal(modalDialog(
+      title = "BAM report",
+      tags$iframe(srcdoc=readLines(file.path(bam_report_dir(),"bam_report.html")), style='width:100%;height:600px;'),
+      easyClose = TRUE,size = "xl",
+      footer = downloadButton("btn_dl_bam_report_html","Download",icon = icon("download"),class="btn-primary")
+    ))
+  })
+  
+  output$btn_dl_bam_report_html <- downloadHandler(
+    filename = function(){paste0(input$bam_file$name,".html")},
+    content = function(file) {file.copy(file.path(bam_report_dir(),"bam_report.html"),file)}
+  )
+  
+  #-#-#-#-#-#-#-#
+  # VCF
+  #-#-#-#-#-#-#-#
+  vcf_report_dir <- reactiveVal(NULL)
+  
+  observeEvent(input$vcf_file,{
+    validate(need(is.character(input$vcf_file$datapath),"invalid VCF"))
+    shinyjs::enable("btn_make_vcf_report")
+  })
+  
+  observeEvent(input$btn_make_vcf_report,{
+    validate(need(is.character(input$vcf_file$datapath),"invalid VCF"))
+    shinyjs::disable("btn_view_vcf_report")
+    shinyjs::disable("btn_dl_vcf_report_docx")
+    
+    # Copy inputs to temp location
+    tempdir <- tempdir()
+    file.copy("notebooks/vcf_report.Rmd", file.path(tempdir,"template.Rmd"), overwrite = TRUE)
+    file.copy(input$vcf_file$datapath, file.path(tempdir,input$vcf_file$name), overwrite = TRUE)
+    file.copy(input$resistance_db, tempdir, overwrite = TRUE,recursive = TRUE)
+    
+    # Generate report
+    withProgress(message="Generate VCF report",{
+      rmarkdown::render(
+        file.path(tempdir,"template.Rmd"),
+        output_file = file.path(tempdir,"vcf_report.html"),
+        params = list(
+          input_vcf_file = file.path(tempdir,input$vcf_file$name),
+          input_db_dir = file.path(tempdir,"db"),
+          output_docx_report = file.path(tempdir,"vcf_report.docx")),
+        envir = new.env(parent = globalenv())
+      )
+    })
+    shinyjs::enable("btn_view_vcf_report")
+    shinyjs::enable("btn_dl_vcf_report_docx")
+    bam_report_dir(tempdir)
+  })
+  
+  observeEvent(input$btn_view_vcf_report,{
+    showModal(modalDialog(
+      title = "VCF report",
+      tags$iframe(srcdoc=readLines(file.path(bam_report_dir(),"vcf_report.html")), style='width:100%;height:600px;'),
+      easyClose = TRUE,size = "xl",
+      footer = downloadButton("btn_dl_vcf_report_html","Download",icon = icon("download"),class="btn-primary")
+    ))
+  })
+  
+  output$btn_dl_vcf_report_html <- downloadHandler(
+    filename = function(){paste0(input$vcf_file$name,".html")},
+    content = function(file) {file.copy(file.path(bam_report_dir(),"vcf_report.html"),file)}
+  )
+  
+  output$btn_dl_vcf_report_docx <- downloadHandler(
+    filename = function(){paste0(input$vcf_file$name,".docx")},
+    content = function(file) {file.copy(file.path(bam_report_dir(),"vcf_report.docx"),file)}
+  )
+  
+  # output$terminal_stdout <- eventReactive(input$make_doc,{
+  #   validate(need(is.character(input$vcf_file$datapath),"invalid fasta"))
+  #   cmd <- sprintf("make %s 2>&1",input$vcf_file$datapath)
+  #   print(cmd)
+  #   withProgress(message="Generate report",{
+  #     pipe(cmd) |> readLines() |> paste(collapse = "\n")
+  #   })
+  # })
+}
+
+shinyApp(ui = ui(),server = server)
